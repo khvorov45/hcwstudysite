e2076c818d78aacea79e8eaff1e7c16a3d0f7c1b
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-02 12:02:03 +1100

    added a buttom to switch themes

---
+++ www/script.js
@@ -0,0 +1,11 @@
+function switch_theme() {
+    var theme_css = document.getElementById("theme-css")
+    var dark_theme = "dark.css"
+    var light_theme = "light.css"
+    if (document.getElementById("dark").checked) {
+        theme_css.href = dark_theme
+    }
+    else {
+        theme_css.href = light_theme
+    }
+}
