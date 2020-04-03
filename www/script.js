75aaa5561d811af540e0f0989bd2a343b3d62086
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-03 12:12:43 +1100

    moved theme switching to button

--- www/script.js
+++ www/script.js
@@ -2,12 +2,10 @@ function switch_theme() {
     var theme_css = document.getElementById("theme-css")
     var dark_theme = "dark.css"
     var light_theme = "light.css"
-    if (document.getElementById("dark").checked) {
-        theme_css.href = dark_theme
-    }
-    else {
+    if (document.getElementById("light").checked) {
         theme_css.href = light_theme
     }
+    else {
+        theme_css.href = dark_theme
+    }
 }
-
-$("[name='my-checkbox']").bootstrapSwitch();
