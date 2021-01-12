5f2474380f3349821473af235d8d8854343b6ad1
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-12 12:14:10 +1100

    style the mardown (paddingcentering)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,25 @@ import Layout from "../components/layout"
 import fs from "fs"
 import ReactMarkdown from "react-markdown"
 
+function Markdown({ content }: { content: string }) {
+  return (
+    <div
+      style={{
+        margin: "auto",
+        maxWidth: 1000,
+        paddingLeft: 20,
+        paddingRight: 20,
+      }}
+    >
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </div>
+  )
+}
+
 export default function Home({ md }: { md: string }) {
   return (
     <Layout active={(s) => s === "/"}>
-      <div style={{ marginLeft: 20 }}>
-        <ReactMarkdown>{md}</ReactMarkdown>
-      </div>
+      <Markdown content={md} />
     </Layout>
   )
 }
