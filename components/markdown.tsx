7f05414ab99a6d4ccece51366391ff9d82208cc8
Arseniy Khvorov <khvorov45@gmail.com>
2021-01-22 08:38:40 +1100

    style the markdown better

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -2,22 +2,28 @@ import {
   createStyles,
   Divider,
   Link as MaterialLink,
+  makeStyles,
   Theme,
   withStyles,
 } from "@material-ui/core"
 import React from "react"
 import ReactMarkdown from "react-markdown"
 
+const useStyles = makeStyles((theme: Theme) => ({
+  md: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    maxWidth: 1000,
+    "&>ul>*": {
+      marginTop: 10,
+    },
+  },
+}))
+
 export default function Markdown({ content }: { content: string }) {
+  const classes = useStyles()
   return (
-    <div
-      style={{
-        margin: "auto",
-        maxWidth: 1000,
-        paddingLeft: 20,
-        paddingRight: 20,
-      }}
-    >
+    <div className={classes.md}>
       <ReactMarkdown renderers={{ link: Link, thematicBreak: Divider }}>
         {content}
       </ReactMarkdown>
