ff598a2807cdde1794089ff1d8dfa5e5a7c09623
Arseniy Khvorov <khvorov45@gmail.com>
2021-03-02 13:24:21 +1100

    add logos

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -8,11 +8,24 @@
 
 <script lang="ts">
   export let md: string
+  import { SITES } from "../lib/config"
 </script>
 
 <div class="md">
   {@html md}
 </div>
+
+<footer>
+  {#each SITES as site}
+    <div class="logo-container {site.short}-container">
+      <img
+        alt="logo-{site.short}"
+        src="logo-{site.short}.png"
+        class="logo-{site.short}"
+      />
+    </div>
+  {/each}
+</footer>
 
 <style>
   .md {
@@ -20,6 +33,48 @@
     max-width: 56em;
     margin-left: auto;
     margin-right: auto;
+  }
+  footer {
+    display: grid;
+    grid-template-areas:
+      "ad ad sd"
+      "br br sd"
+      "ml nc pr";
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+  }
+  img {
+    filter: invert(var(--invert-img));
+    transition: filter var(--time-transition);
+    max-width: 100%;
+    max-height: 100%;
+  }
+  .adelaide-container {
+    grid-area: ad;
+    max-width: 500px;
+    height: 80px;
+  }
+  .brisbane-container {
+    grid-area: br;
+    max-width: 500px;
+    height: 55px;
+  }
+  .melbourne-container {
+    grid-area: ml;
+    height: 72px;
+  }
+  .newcastle-container {
+    grid-area: nc;
+    height: 72px;
+  }
+  .perth-container {
+    grid-area: pr;
+    height: 72px;
+  }
+  .sydney-container {
+    grid-area: sd;
+    height: 135px;
   }
   :global(li) {
     margin-top: 5px;
